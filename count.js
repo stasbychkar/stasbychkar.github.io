2b2fb6e01e660ce1d0313b2a7d87ed7b58a1747a
stasbychkar <stanislav.bychkar@MacBook-Air-Stanislav.local>
2024-04-10 15:01:58 -0700

    2nd commit

--- count.js
+++ count.js
@@ -1,3 +1,43 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // count total cash
+    document.querySelector('form').onsubmit = () => {
+        let cash_sum = 0;
+        document.querySelectorAll('.coins').forEach((input) => {
+            cash_sum += count(input.value) * parseFloat(input.dataset.coin);
+        });
+        let result = document.querySelector('#result');
+        result.innerHTML = cash_sum.toFixed(2);
+
+        return false;
+    
+    }
+
+    // sync count -> amount
+    document.querySelectorAll('[data-count]').forEach((inputField) => {
+        inputField.addEventListener('change', () => {
+            let amount = count(inputField.value) * parseFloat(inputField.dataset.coin);
+            let inputClass = inputField.className.split(' ')[1];
+            document.querySelectorAll("." + inputClass)[1].value = amount.toFixed(2);
+        });
+    });
+    
+    // sync amount -> count
+    document.querySelectorAll('[data-amount]').forEach((inputField) => {
+        inputField.addEventListener('change', () => {
+            let inputClass = inputField.className.split(' ')[1];
+            let countField = document.querySelectorAll("." + inputClass)[0];
+            let amount = count(inputField.value) / parseFloat(countField.dataset.coin);
+            document.querySelectorAll("." + inputClass)[0].value = amount;
+        });
+    });
+
+    // bills selection
+    // choose drawer
+})
+
+// helper fubnctions
+
 // dealing with a single input
 function count(input) {
     if (!input) {
@@ -18,28 +58,3 @@ function count(input) {
         }
     }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelector('form').onsubmit = () => {
-        let cash_sum = 0;
-        document.querySelectorAll('.coins').forEach((input) => {
-            cash_sum += count(input.value) * parseFloat(input.dataset.coin);
-        });
-        let result = document.querySelector('#result');
-        result.innerHTML = cash_sum.toFixed(2);
-
-        return false;
-    
-    }
-
-    document.querySelectorAll('[data-count]').forEach((inputField) => {
-        inputField.addEventListener('change', () => {
-            let amount = count(inputField.value) * parseFloat(inputField.dataset.coin);
-            let inputClass = inputField.className.split(' ')[1];
-            document.querySelectorAll("." + inputClass)[1].value = amount.toFixed(2);
-        });
-    });
-    
-    
-})
-
